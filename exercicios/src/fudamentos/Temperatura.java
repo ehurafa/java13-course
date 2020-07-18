ba76ecdfbb618086639fa8322d24183f4f53842c
Rafael Gomes <rafarfl2010@gmail.com>
2020-07-18 19:42:50 -0300

    desafio variaveis

---
+++ exercicios/src/fudamentos/Temperatura.java
@@ -0,0 +1,17 @@
+package fudamentos;
+
+public class Temperatura {
+	public static void main(String[] args) {
+		// (°F - 32) x 5/9 = °C
+		
+		final double AJUSTE = 32;
+		final double FATOR = 5.0/9;
+		
+		double fahrenheit = 86;		
+		double celsius = (fahrenheit - AJUSTE) * FATOR;
+			
+			System.out.println(celsius);
+		
+		
+	}
+}
