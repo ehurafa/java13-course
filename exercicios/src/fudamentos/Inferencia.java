ff0383449338f92a712f67f234f742ef0ace82b4
Rafael Gomes <rafarfl2010@gmail.com>
2020-07-21 23:26:15 -0300

    inferindo tipos

---
+++ exercicios/src/fudamentos/Inferencia.java
@@ -0,0 +1,22 @@
+package fudamentos;
+
+public class Inferencia {
+	
+	public static void main(String[] args) {
+		double a = 4.5;
+		System.out.println(a);
+		
+		var b = "foo";
+		
+		System.out.println(b);
+		
+		var c = 56;
+		System.out.println(c);
+		
+		double d;
+		d = 15566.6;
+		
+		System.out.println(d);
+	}
+	
+}
