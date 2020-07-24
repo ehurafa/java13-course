892366e6c19ebd89287c51f0c7ae1b484d17b270
Rafael Gomes <rafarfl2010@gmail.com>
2020-07-23 23:38:45 -0300

    notação ponto

---
+++ exercicios/src/fudamentos/NotacaoPontos.java
@@ -0,0 +1,21 @@
+package fudamentos;
+
+public class NotacaoPontos {
+
+	public static void main(String[] args) {
+		
+		double a = 2.3;
+		String s = "Boa noite";
+		s = s.replace("noite", "tarde");
+		s = s.toUpperCase();	
+		s = s.concat("!");
+		System.out.println(s);
+		
+		String x = "Rafa".toLowerCase();
+		System.out.println(x);
+		
+		// tipos primitivos não tem o operador "."
+		int b = 3;
+		//a.
+	}
+}
