d87bcfe20baa57b58ad3896af7aacf2d8494c95c
Rafael Gomes <rafarfl2010@gmail.com>
2020-07-23 23:52:25 -0300

    imports

---
+++ exercicios/src/fudamentos/Import.java
@@ -0,0 +1,18 @@
+package fudamentos;
+
+import java.util.Date;
+
+public class Import {
+
+	public static void main(String[] args) {
+		
+		java.lang.String a = "Bom dia";
+		java.lang.System.out.println(a);
+		
+		
+		Date d = new Date();
+		System.out.println(d);
+		
+		//JButton botao = new JButton();
+	} 
+}
