f6c93d6a87bb7c979fd8f9cc694c6189c2207715
Rafael Gomes <rafarfl2010@gmail.com>
2020-08-04 23:48:53 -0300

    tipo string

---
+++ exercicios/src/fudamentos/TipoString.java
@@ -0,0 +1,11 @@
+package fudamentos;
+
+public class TipoString {
+
+	public static void main(String[] args) {
+		System.out.println("Olá pessoal".charAt(0));
+		
+		String s = "boa tarde";
+		System.out.println(s.concat("!!!"));
+	}
+}
