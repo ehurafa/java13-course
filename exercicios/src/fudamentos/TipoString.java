a5cddabc318ffaa5fd0788684fde3c5a70644e12
Rafael Gomes <rafarfl2010@gmail.com>
2020-08-11 23:20:16 -0300

    continuação string

--- exercicios/src/fudamentos/TipoString.java
+++ exercicios/src/fudamentos/TipoString.java
@@ -5,7 +5,31 @@ public class TipoString {
 	public static void main(String[] args) {
 		System.out.println("Olá pessoal".charAt(0));
 		
-		String s = "boa tarde";
+		String s = "Boa tarde";
 		System.out.println(s.concat("!!!"));
+		System.out.println(s.startsWith("Boa"));
+		System.out.println(s.toLowerCase().startsWith("boa"));
+		System.out.println(s.endsWith("tarde"));
+		System.out.println(s.length());
+		System.out.println(s.equals("boa tarde"));
+		System.out.println(s.equalsIgnoreCase("boa tarde"));
+		
+		var nome = "Pedro";		
+		var sobrenome = "Santos";
+		var idade = 33;
+		var salario = 987.89;
+		
+		System.out.println("Nome: " + nome + "\nSobrenome: " + sobrenome + "\nIdade: " + idade + "\nSalário: " + salario + "\n\n");
+		
+		System.out.printf("O senhor %s %s tem %d e ganha R$%.2f.", nome, sobrenome, idade, salario);
+		
+		String frase = String.format("\nO senhor %s %s tem %d e ganha R$%.2f.", nome, sobrenome, idade, salario);
+		
+		System.out.println(frase);
+		
+		System.out.println("Frase qualquer".equals(frase));
+		System.out.println("Frase qualquer".indexOf("qual"));
+		System.out.println("Frase qualquer".substring(6));
+		System.out.println("Frase qualquer".substring(6,8));
 	}
 }
