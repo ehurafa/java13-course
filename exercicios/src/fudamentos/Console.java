75a03213ad020cd5cb749f8eb4f00b788cea556e
Rafael Gomes <rafarfl2010@gmail.com>
2020-08-13 23:01:15 -0300

    console

---
+++ exercicios/src/fudamentos/Console.java
@@ -0,0 +1,36 @@
+package fudamentos;
+
+import java.util.Scanner;
+
+public class Console {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Bom");
+		System.out.print(" dia\n");
+		
+		System.out.println("Bom");
+		System.out.println("dia");
+		
+		System.out.printf("Megasena: %d %d %d %d ", 1, 2, 5, 5);
+		System.out.printf("Salário: %.1f%n", 1234.5678);
+		System.out.printf("Nome: %s%n", "João");
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Digite o seu nome: ");		
+		String nome = entrada.nextLine();
+		
+		System.out.print("Digite o seu sobrenome: ");		
+		String sobrenome = entrada.nextLine();
+		
+		System.out.print("Digite a sua idade: ");		
+		int idade = entrada.nextInt();
+		
+		System.out.println("\n\n Nome = " + nome + " " + sobrenome);
+		System.out.printf("%s %s tem %d anos.",
+				nome, sobrenome, idade);
+		
+		entrada.close();
+	}
+}
