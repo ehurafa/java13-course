bbeb4157ae9c625858650ae3f50cbb46584608d3
Rafael Gomes <rafarfl2010@gmail.com>
2020-08-15 23:19:30 -0300

    wrappers

---
+++ exercicios/src/fudamentos/Wrapper.java
@@ -0,0 +1,37 @@
+package fudamentos;
+
+//import java.util.Scanner;
+
+public class Wrapper {
+
+	public static void main(String[] args) {
+		
+		//Scanner entrada = new Scanner(System.in);
+		
+		
+		Byte b = 100;
+		Short s = 1000;
+		//Integer i = Integer.parseInt(entrada.next());
+		Integer i = Integer.parseInt("1000");
+		Long l = 100000L;
+		
+		System.out.println(b.byteValue());
+		System.out.println(i * 3);
+		System.out.println(l * 3);
+		
+		Float f = 123.10F;
+		System.out.println(f);
+		
+		Double d = 1234.5678;
+		System.out.println(d);
+		
+		Boolean bo = Boolean.parseBoolean("true");
+		System.out.println(bo);
+		System.out.println(bo.toString().toUpperCase());
+		
+		Character c = '#'; // char
+		System.out.println(c + "...");
+		
+		//entrada.close();
+	}
+}
