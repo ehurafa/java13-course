77ebe005fa013149c768e0bdf5e7efb602696848
Rafael Gomes <rafarfl2010@gmail.com>
2020-08-25 21:20:20 -0300

    cast

---
+++ exercicios/src/fudamentos/ConversaoTiposPrimitivosNumericos.java
@@ -0,0 +1,22 @@
+package fudamentos;
+
+public class ConversaoTiposPrimitivosNumericos {
+	
+	public static void main(String[] args) {
+		
+		double a = 1;  //implicita
+		System.out.println(a);
+		
+		float b = (float) 1.12345;  //explicita (CAST)
+		System.out.println(b); 
+		
+		int c = 4;
+		byte d = (byte) c;
+		System.out.println(d);
+		
+		double e = 1;
+		int f = (int) e;
+		System.out.println(f);
+	}
+
+}
