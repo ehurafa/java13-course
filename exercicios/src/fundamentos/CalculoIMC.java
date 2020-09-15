53205aeb8a64e4e1790850ff7296eee983d02e3f
Rafael Gomes <rafarfl2010@gmail.com>
2020-09-14 21:15:00 -0300

    calculo IMC

---
+++ exercicios/src/fundamentos/CalculoIMC.java
@@ -0,0 +1,24 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class CalculoIMC {
+	
+	public static void main(String[] args) {
+	 
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite o peso:");
+		double peso = scanner.nextDouble();
+		
+		System.out.println("Digite a altura:");
+		double altura = scanner.nextDouble();
+		
+		double imc = peso / ( altura * altura );
+		
+		System.out.print("IMC: " + imc);
+		
+		scanner.close();
+	}
+
+}
